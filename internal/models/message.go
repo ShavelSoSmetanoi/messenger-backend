4463c8491e3fc5565737bdd85608e1afa7d2e28d
Roman <romashka@pishi_v_tg.com>
2024-10-03 22:43:00 +0300

    update model

---
+++ internal/models/message.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Message struct {
+	ID        int       `json:"id"`
+	ChatID    int       `json:"chat_id"`
+	UserID    string    `json:"user_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	IsRead    bool      `json:"is_read"`
+	ReadAt    time.Time `json:"read_at"`
+}
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,11 @@
 package models
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Photo    []byte `json:"photo"`
+	UniqueId string `json:"unique_id"`
+	About    string `json:"about"`
+}
