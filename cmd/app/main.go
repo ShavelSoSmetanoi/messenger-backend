d6f85372c0fde7db06b3fb1da563617fec2e41a2
Roman <romashka@pishi_v_tg.com>
2024-10-04 20:44:27 +0300

    add Dockerfile

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	r := gin.Default() // Создаём новый роутер
+
+	// Определяем маршрут
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	// Запускаем сервер на порту 8080
+	r.Run(":8080")
 }
