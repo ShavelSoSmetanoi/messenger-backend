5f26e4a4e52a9893bee421602b4eadf325bb4725
Roman <romashka@pishi_v_tg.com>
2024-10-08 10:36:45 +0300

    add jwt

--- pkg/JWT/jwt.go
+++ pkg/JWT/jwt.go
@@ -1,1 +1,26 @@
 package JWT
+
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"os"
+	"time"
+)
+
+func CreateJWT(userID string) (string, error) {
+	var jwtKey = []byte(os.Getenv("JWT_SECRET"))
+	// Создание нового токена с использованием HS256 алгоритма подписи
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Установка клеймов (payload) токена
+	claims := token.Claims.(jwt.MapClaims)
+	claims["userID"] = userID
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix() // Токен действителен в течение 24 часов
+
+	// Подписание токена с использованием секретного ключа
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
