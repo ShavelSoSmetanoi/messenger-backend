47a40dc523d7f3ee68f2161400b0fa802a751948
Roman <romashka@pishi_v_tg.com>
2024-10-24 14:38:44 +0300

    add goose migrations

---
+++ migrations/20241024094935_add_users_table.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    photo BYTEA,
+    unique_id VARCHAR(255) NOT NULL UNIQUE,
+    about TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +goose Down
+DROP TABLE users;
