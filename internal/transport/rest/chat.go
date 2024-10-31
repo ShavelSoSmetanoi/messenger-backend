883fd0adb52794a7f93046c9850a3ce5b3a4ca58
Roman <romashka@pishi_v_tg.com>
2024-10-31 17:32:55 +0300

    add chat and message routes

---
+++ internal/transport/rest/chat.go
@@ -0,0 +1,10 @@
+package rest
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) InitChatRouter(r *gin.RouterGroup) {
+
+	// TODO r.POST("/chats", ...)
+	// TODO r.GET("/chats", ...)
+	// TODO r.DELETE("/chats/:chat_id",...)
+}
---
+++ internal/transport/rest/message.go
@@ -0,0 +1,9 @@
+package rest
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) InitMessageRouter(r *gin.RouterGroup) {
+
+	// TODO r.POST("/chats/:chat_id/messages", ...)
+	// TODO r.GET("/chats/:chat_id/messages", ...)
+}
