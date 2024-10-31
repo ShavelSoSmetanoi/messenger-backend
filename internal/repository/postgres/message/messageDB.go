ab037003c640656cc8040e594f11b1417690bafb
Roman <romashka@pishi_v_tg.com>
2024-10-31 17:12:19 +0300

    add  MessageRepository

--- internal/repository/postgres/message/messageDB.go
+++ internal/repository/postgres/message/messageDB.go
@@ -24,23 +24,57 @@ func NewPostgresMessageRepository(db *pgxpool.Pool) *PostgresMessageRepository {
 	return &PostgresMessageRepository{DB: db}
 }
 
-// CreateMessage creates a new message in the chat
+// CreateMessage создает новое сообщение в чате
 func (r *PostgresMessageRepository) CreateMessage(ctx context.Context, message *models.Message) error {
-	// TODO: Implement the logic for creating a new message
-	log.Println("CreateMessage not implemented yet")
+	query := `INSERT INTO messages (chat_id, user_id, content, created_at) 
+              VALUES ($1, $2, $3, $4)`
+	_, err := r.DB.Exec(ctx, query, message.ChatID, message.UserID, message.Content, message.CreatedAt)
+	if err != nil {
+		log.Printf("Error creating message: %v", err)
+		log.Printf("Message details: ChatID=%d, UserID=%d, Content=%s, CreatedAt=%v",
+			message.ChatID, message.UserID, message.Content, message.CreatedAt)
+		return err
+	}
+
 	return nil
 }
 
-// GetMessagesByChatID retrieves all messages in a chat by chat ID
+// GetMessagesByChatID возвращает все сообщения в чате
 func (r *PostgresMessageRepository) GetMessagesByChatID(ctx context.Context, chatID int) ([]models.Message, error) {
-	// TODO: Implement the logic for retrieving messages by chat ID
-	log.Println("GetMessagesByChatID not implemented yet")
-	return nil, nil
+	query := `SELECT id, chat_id, user_id, content, created_at 
+              FROM messages WHERE chat_id = $1`
+	rows, err := r.DB.Query(ctx, query, chatID)
+	if err != nil {
+		log.Printf("Error retrieving messages for ChatID=%d: %v", chatID, err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var messages []models.Message
+	for rows.Next() {
+		var message models.Message
+		if err := rows.Scan(&message.ID, &message.ChatID, &message.UserID, &message.Content, &message.CreatedAt); err != nil {
+			log.Printf("Error scanning message: %v", err)
+			return nil, err
+		}
+		messages = append(messages, message)
+	}
+
+	return messages, nil
 }
 
-// IsUserInChat checks if a user is a participant in a chat
+// IsUserInChat проверяет, является ли пользователь участником чата
 func (r *PostgresMessageRepository) IsUserInChat(ctx context.Context, chatID int, userID int) (bool, error) {
-	// TODO: Implement the logic for checking if a user is in the chat
-	log.Println("IsUserInChat not implemented yet")
-	return false, nil
+	var exists bool
+	query := `SELECT EXISTS (
+                SELECT 1 
+                FROM chatparticipants 
+                WHERE chat_id = $1 AND user_id = $2)`
+	err := r.DB.QueryRow(ctx, query, chatID, userID).Scan(&exists)
+	if err != nil {
+		log.Printf("Error checking if user %d is in chat %d: %v", userID, chatID, err)
+		return false, err
+	}
+
+	return exists, nil
 }
