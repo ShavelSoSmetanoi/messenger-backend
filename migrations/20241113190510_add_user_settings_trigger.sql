e668be3a185866c3c0ad3142a7ef4911899f84a9
Roman <romashka@pishi_v_tg.com>
2024-11-14 16:23:35 +0300

    fix bug user_settings_trigger.sql

--- migrations/20241113190510_add_user_settings_trigger.sql
+++ migrations/20241113190510_add_user_settings_trigger.sql
@@ -1,17 +1,20 @@
 -- +goose Up
+
+-- +goose StatementBegin
 CREATE OR REPLACE FUNCTION create_user_settings()
-    RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS $_$
 BEGIN
     INSERT INTO user_settings (user_id)
     VALUES (NEW.id);
     RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+$_$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_user_insert
     AFTER INSERT ON users
     FOR EACH ROW
 EXECUTE FUNCTION create_user_settings();
+-- +goose StatementEnd
 
 -- +goose Down
 DROP TRIGGER IF EXISTS after_user_insert ON users;
