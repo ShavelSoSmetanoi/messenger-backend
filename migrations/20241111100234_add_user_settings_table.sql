80bc3b3becd4ddc00d703e6554f7c4aa600cc145
Roman <romashka@pishi_v_tg.com>
2024-11-16 19:32:19 +0300

    change default settings

--- migrations/20241111100234_add_user_settings_table.sql
+++ migrations/20241111100234_add_user_settings_table.sql
@@ -2,8 +2,8 @@
 CREATE TABLE IF NOT EXISTS user_settings (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    theme VARCHAR(50) DEFAULT 'light',
-    message_color VARCHAR(7) DEFAULT '#0000FF',
+    theme VARCHAR(50) DEFAULT '#121212',
+    message_color VARCHAR(7) DEFAULT '#32cd32',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
